f4498a6692c89c596e3f4de80975e4bf14944dda
Again <aashish.amgain@gmail.com>
2023-09-22 19:28:10 +0545

    Added User Input for Niche and Location and optimized

--- LeadScout.py
+++ LeadScout.py
@@ -2,74 +2,83 @@ import requests
 from bs4 import BeautifulSoup
 import concurrent.futures
 
-# Define the city and niche to scrape
-city = "San Francisco"
-niche = "Home Decor"
+# Get the niche and location from the user
+niche = input("Enter a niche: ")
+location = input("Enter a location: ")
 
-url = "https://www.google.com/search?q={}+{}".format(city, niche)
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
+# Generate a list of keywords related to the niche
+keywords = [
+    niche,
+    niche + " " + location,
+    niche + " services near me",
+    niche + " companies near me",
+    niche + " businesses near me",
+    niche + " contractors near me",
+    niche + " professionals near me",
+    niche + " experts near me",
+    niche + " reviews",
+    niche + " ratings",
+    niche + " directory",
+    niche + " listings"
+]
 
-businesses = []
-for business in soup.findAll("div", class_="cX8d1e"):
-    name = business.find("span", class_="BNeawe").text
-    address = business.find("span", class_="BNeawe tA6K1").text
-    phone_number = business.find("span", class_="phoneNumber").text
-    email_address = business.find("span", class_="email").text
+# Scrape data for each keyword in parallel
+def scrape_data(keyword):
+    # Get the list of businesses from Google
+    url = "https://www.google.com/search?q={}+{}".format(keyword, location)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    social_media_links = []
-    for link in business.findAll("a", class_="BNeawe"):
-        href = link.get("href")
-        if "facebook" in href:
-            social_media_links.append({"platform": "Facebook", "url": href})
-        elif "instagram" in href:
-            social_media_links.append({"platform": "Instagram", "url": href})
-        elif "tiktok" in href:
-            social_media_links.append({"platform": "TikTok", "url": href})
+    # Extract the business name, address, phone number, email address, and social media links
+    businesses = []
+    for business in soup.findAll("div", class_="cX8d1e"):
+        name = business.find("span", class_="BNeawe").text
+        address = business.find("span", class_="BNeawe tA6K1").text
+        phone_number = business.find("span", class_="phoneNumber").text
+        email_address = business.find("span", class_="email").text
 
+        # Extract social media links
+        social_media_links = []
+        for link in business.findAll("a", class_="BNeawe"):
+            href = link.get("href")
+            if "facebook" in href:
+                social_media_links.append({"platform": "Facebook", "url": href})
+            elif "instagram" in href:
+                social_media_links.append({"platform": "Instagram", "url": href})
+            elif "tiktok" in href:
+                social_media_links.append({"platform": "TikTok", "url": href})
 
-    businesses.append({
-        "name": name,
-        "address": address,
-        "phone_number": phone_number,
-        "email_address": email_address,
-        "social_media_links": social_media_links
-    })
+        # Extract the business's website
+        website = business.find("a", class_="BNeawe tA6K1")
+        website = website.get("href")
 
-# Scrape additional data, such as reviews, website, operating since, and employee number
-def scrape_additional_data(business):
-    website = None
-    for link in business["social_media_links"]:
-        if "website" in link["platform"]:
-            website = link["url"]
-            break
+        # Scrape reviews from the business's website
+        # (This code will vary depending on the website)
+        reviews = []
+        # ...
 
-    
-    reviews = []
-    if website:
-        
-        pass
+        # Add the business to the list of businesses
+        businesses.append({
+            "name": name,
+            "address": address,
+            "phone_number": phone_number,
+            "email_address": email_address,
+            "social_media_links": social_media_links,
+            "website": website,
+            "reviews": reviews
+        })
 
-    
-    operating_since = None
-    
+    return businesses
 
-    
-    employee_number = None
-    
-
-    
-    business["website"] = website
-    business["reviews"] = reviews
-    business["operating_since"] = operating_since
-    business["employee_number"] = employee_number
-
+# Scrape data for each keyword in parallel
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    results = executor.map(scrape_additional_data, businesses)
+    results = executor.map(scrape_data, keywords)
 
+# Export the data to CSV
 import csv
 with open("businesses.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["name", "address", "phone_number", "email_address", "social_media_links", "website", "reviews", "operating_since", "employee_number"])
-    for business in businesses:
-        writer.writerow([business["name"], business["address"], business["phone_number"], business["email_address"], ",".join([link["url"] for link in business["social_media_links"]]), business["website"], ",".join(business["reviews"]), business["operating_since"], business["employee_number"]])
+    writer.writerow(["name", "address", "phone_number", "email_address", "social_media_links", "website", "reviews"])
+    for businesses in results:
+        for business in businesses:
+            writer.writerow([business["name"], business["address"], business["phone_number"], business["email_address"], ",".join([link["url"] for link in business["social_media_links"]]), business["website"], ",".join(business["reviews"])])
